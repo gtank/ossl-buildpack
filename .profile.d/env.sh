af0b37d9251bc670aaf867c2113305e32dfbf8ca
George Tankersley <george.tankersley@gmail.com>
2013-06-26 23:36:30 -0700

    try setting env vars another way

---
+++ .profile.d/env.sh
@@ -0,0 +1,4 @@
+export PATH=/app/vendor/ruby/bin:$PATH
+export RUBYLIB=/app/vendor/ruby/lib/ruby/2.0.0/:/app/vendor/ruby/lib/ruby/2.0.0/x86_64-linux/
+export GEM_PATH=/app/vendor/ruby/lib/ruby/gems/2.0.0/
+export LD_LIBRARY_PATH=/app/vendor/openssl/lib/
